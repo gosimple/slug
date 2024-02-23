4a8e3598e11f25d3257a89657df69e9c7fb09060
Rafael Passos <rafael@rcpassos.me>
2024-02-23 16:44:14 -0300

    Adds portuguese language & @ symbols (#79)

--- slug.go
+++ slug.go
@@ -93,6 +93,8 @@ func MakeLang(s string, lang string) (slug string) {
 		slug = SubstituteRune(slug, nnSub)
 	case "pl", "pol":
 		slug = SubstituteRune(slug, plSub)
+	case "pt", "prt", "pt-br", "bra", "por":
+		slug = SubstituteRune(slug, ptSub)
 	case "ro", "rou":
 		slug = SubstituteRune(slug, roSub)
 	case "sl", "slv":
